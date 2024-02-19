f7f58de432d0576b0339c0567ae851bc08479501
malilishadddy <124892968+malilishadddy@users.noreply.github.com>
2024-02-19 14:02:08 +0300

    Add files via upload

---
+++ Webdevelopment/Task2/script.js
@@ -0,0 +1,16 @@
+
+    function appendToResult(value) {
+        document.getElementById("result").value += value;
+    }
+
+    function clearResult() {
+        document.getElementById("result").value = '';
+    }
+
+    function calculate() {
+        try {
+            document.getElementById("result").value = eval(document.getElementById("result").value);
+        } catch (error) {
+            document.getElementById("result").value = 'Error';
+        }
+    }
