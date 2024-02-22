897fab6057c89fe47511b99ce3633e1970f82423
malilishaddy <malilishaddy@gmail.com>
2024-02-22 21:12:51 +0300

    Second commit

--- Task3/job_board/src/App.jsx
+++ Task3/job_board/src/App.jsx
@@ -1,12 +1,15 @@
 
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  
 
   return (
     <>
-    <h1>Hello</h1>
+    <nav>Navbar</nav>
+    <Outlet/>
+    <footer>Footer</footer>
+
     </>
   )
 }
